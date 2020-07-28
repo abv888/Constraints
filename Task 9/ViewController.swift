b88e47527c2f36c82c826d6ede50bf4a7e0113cf
Bagrat Arutyunov <Arutyunovbagrat@gmail.com>
2020-07-28 11:52:00 +0300

    Initial Commit

---
+++ Task 9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 9
+//
+//  Created by  Bagrat Arutyunov on 28.07.2020.
+//  Copyright © 2020  Bagrat Arutyunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
